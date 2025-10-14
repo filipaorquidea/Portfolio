270bc8f5d22d6f17fdd94eaa431ef2b73de34696
filipaorquidea <uc2020231259@student.uc.pt>
2025-10-14 17:15:35 +0100

    alteracoes

--- trabalhos.js
+++ trabalhos.js
@@ -1,4 +1,4 @@
-const numDivs = 10;
+const numDivs = 20;
 
 for (let i = 0; i < numDivs; i++) {
   createDiv();
@@ -7,7 +7,7 @@ for (let i = 0; i < numDivs; i++) {
 function createDiv() {
   const work = document.createElement('div');
   work.classList.add('work');
-  work.appendChild(document.createTextNode('WORK'));
+  work.appendChild(document.createTextNode('Filipa Orquídea'));
 
   const randomX = Math.random() * (window.innerWidth - 150);
   const randomY = Math.random() * (window.innerHeight + 350);
